7c4863c3aa8581880c9e01101fc4d673c2179c12
bittu9835 <bk2671111@gmail.com>
2024-01-04 18:47:26 +0530

    add

--- suvaidyam/suvaidyam/doctype/vehicle/vehicle.js
+++ suvaidyam/suvaidyam/doctype/vehicle/vehicle.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2023, Bittu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Vehicle", {
-// 	refresh(frm) {
+frappe.ui.form.on('Vehicle', {
+    // Consider using a later event like 'render' or 'after_render' if necessary
+    refresh: function (frm) {
+        // Use console.log for debugging
+        console.log("Setting placeholders for make and model fields");
 
-// 	},
-// });
+        // Ensure correct field IDs
+        var makeField = $("#make");
+        var modelField = $("#model");
+        console.log(makeField)
+
+        // Set placeholders with appropriate text
+        makeField.attr("placeholder", "Enter vehicle make");
+        modelField.attr("placeholder", "Enter vehicle model");
+    }
+});
